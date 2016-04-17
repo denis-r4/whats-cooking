722178f8cb74f4aa8c2adb3bd74c02a547905ca0
Denis <den4ik.j@gmail.com>
2016-04-17 21:42:48 +0300

    add model proto

--- model/naive_bayes.py
+++ model/naive_bayes.py
@@ -1,1 +1,25 @@
 __author__ = 'denis'
+
+
+class MultinomialNaiveBayes(object):
+    def __init__(self, alpha = 1.0):
+
+        self.__alpha = alpha
+
+    def load_model(self, file_name):
+        pass
+
+    def save_model(self, file_name):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def __str__(self):
+        description = "Here is a description of NB and some statistic..."
+        return description
+
+
